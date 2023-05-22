4f5f31b86ec1ce78701477a3ce4a00cc03d76f6f
Konstantin Merezhnikov <KAMerezhnikov@greenatom.ru>
2023-05-22 20:30:12 +0300

    Fix

--- ticket/urls.py
+++ ticket/urls.py
@@ -4,6 +4,6 @@ from ticket import views
 
 urlpatterns = [
     path('ticket', views.TicketCreateAPIView.as_view()),
-    path('ticket', views.TicketListAPIView.as_view()),
+    path('tickets', views.TicketListAPIView.as_view()),
     path('ticket/<int:pk>', views.TicketRetrieveUpdateDestroyAPIView.as_view()),
 ]
