1116b2f08a31e3cb9cace0feddea3e998a0e0abd
Konstantin Merezhnikov <KAMerezhnikov@greenatom.ru>
2023-05-22 20:39:24 +0300

    New ticker serializer

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 from ticket import models
+from session import serializers as session_serializer
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    session = session_serializer.SessionSerializer(write_only=True)
+    price = session_serializer.PriceListSerializer(write_only=True)
+
     class Meta:
         model = models.Ticket
-        fields = '__all__'
+        fields = ['id', 'row', 'seat', 'session', 'price']
